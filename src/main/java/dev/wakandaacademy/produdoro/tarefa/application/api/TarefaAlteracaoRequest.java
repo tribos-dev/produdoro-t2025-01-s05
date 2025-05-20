ac84646ed8da79431d64b10c1872d3ebfb278974
Rodrigo Silva <rodrigosilvaro46@gmail.com>
2025-05-20 18:25:56 -0300

    test: implementa teste unitario

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAlteracaoRequest.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAlteracaoRequest.java
@@ -1,10 +1,12 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
+@AllArgsConstructor
 public class TarefaAlteracaoRequest {
     @NotBlank
     @Size(message = "Campo descrição tarefa não pode estar vazio", max = 255, min = 3)
