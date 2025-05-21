d86ae12724ed65485980920ab0a00faa23dc88ef
Augusto Andrade <gutoandrade01@gmail.com>
2025-05-21 20:37:33 -0300

    feat:implementa camada Service

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaService.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaService.java
@@ -2,10 +2,13 @@ package dev.wakandaacademy.produdoro.tarefa.application.service;
 
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
+import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaUsuarioListResponse;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 
+import java.util.List;
 import java.util.UUID;
 public interface TarefaService {
     TarefaIdResponse criaNovaTarefa(TarefaRequest tarefaRequest);
     Tarefa detalhaTarefa(String usuario, UUID idTarefa);
+    List<TarefaUsuarioListResponse> listaTodasTarefasDoUsuario(String email, UUID idUsuario);
 }
