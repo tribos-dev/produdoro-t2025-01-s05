636f523648241b8dff510dcc76faf44fd889d85b
Pedro Santos <pedrooohsss@gmail.com>
2025-05-22 19:01:26 -0300

    style: apaga linhas desnecessarias

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationService.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationService.java
@@ -62,6 +62,4 @@ public class UsuarioApplicationService implements UsuarioService {
 		usuarioRepository.salva(usuario);
 		log.info("[finaliza] UsuarioApplicationService - mudaStatusParaPausaCurta");
 	}
-
-
 }
