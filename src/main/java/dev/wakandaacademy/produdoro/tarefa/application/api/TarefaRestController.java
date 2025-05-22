a3ec9ba98d61066c67fdba4385510e920cd524df
Ronan Lima <ronanlimasilva@hotmail.com>
2025-05-22 20:53:36 -0300

    feat:implementa controller

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
@@ -35,6 +35,14 @@ public class TarefaRestController implements TarefaAPI {
 		return new TarefaDetalhadoResponse(tarefa);
 	}
 
+	@Override
+	public void patchConcluiTarefa(String token, UUID idTarefa) {
+		log.info("[inicia] TarefaRestController - patchConcluiTarefa");
+		String usuario = getUsuarioByToken(token);
+		tarefaService.concluiTarefa(usuario,idTarefa);
+		log.info("[finaliza] TarefaRestController - patchConcluiTarefa");
+	}
+
 	private String getUsuarioByToken(String token) {
 		log.debug("[token] {}", token);
 		String usuario = tokenService.getUsuarioByBearerToken(token).orElseThrow(() -> APIException.build(HttpStatus.UNAUTHORIZED, token));
