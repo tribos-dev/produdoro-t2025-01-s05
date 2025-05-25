c7103d99d1e30c5c9d93b556de0aaa9822358dd2
Pedro Santos <pedrooohsss@gmail.com>
2025-05-25 11:59:08 -0300

    fix: corrige bugs

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/repository/TarefaRepository.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/repository/TarefaRepository.java
@@ -10,6 +10,5 @@ public interface TarefaRepository {
 
     Tarefa salva(Tarefa tarefa);
     Optional<Tarefa> buscaTarefaPorId(UUID idTarefa);
-    
     List<Tarefa> buscaTarefasDoUsuario(UUID idUsuario);
 }
